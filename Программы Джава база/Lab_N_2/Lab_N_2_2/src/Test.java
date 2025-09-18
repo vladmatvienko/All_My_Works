8a65c51fc776c87ff61eb991a5f586b06a1d34d8
Владислав Матвиенко <114211276+vladmatvienko@users.noreply.github.com>
2025-09-18 20:39:02 +0300

    Все работы, написанные на JAVA

--- Программы Джава база/Lab_N_2/Lab_N_2_2/src/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        String sc = "Booster_2343";
-        char[] ray = sc.toCharArray();
-        System.out.println(ray[1]);
-    }
-}
